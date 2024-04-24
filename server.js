8d7fd330059f484d3923cad1d84a3b5ad74db539
Bjørn <bm.saastad@gmail.com>
2024-04-24 23:04:20 +0200

    first commit 🚀
    Dream App by Fireship but with Stability AI

---
+++ server.js
@@ -0,0 +1,49 @@
+import fs from "node:fs";
+import axios from "axios";
+import FormData from "form-data";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/dream', async (req, res) => {
+  try {
+    const formData = {
+      prompt: "Lighthouse on a cliff overlooking the ocean",
+      output_format: "webp"
+    };
+    
+    const response = await axios.postForm(
+      `https://api.stability.ai/v2beta/stable-image/generate/core`,
+      axios.toFormData(formData, new FormData()),
+      {
+        validateStatus: undefined,
+        responseType: "arraybuffer",
+        headers: { 
+          Authorization: `Bearer `+process.env.STABILITYAI, 
+          Accept: "application/json" 
+        },
+      },
+    );
+    
+    if(response.status === 200) {
+      //const data = response.json()
+      res.send(response.data.toString())
+      //fs.writeFileSync("./lighthouse.webp", Buffer.from(response.data));
+    } else {
+      throw new Error(`${response.status}: ${response.data.toString()}`);
+    }
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+  }
+});
+
+app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
