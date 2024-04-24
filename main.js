5a7faa8b454ffac2efcd9262050a64b20950e2b9
Bjørn <bm.saastad@gmail.com>
2024-04-24 23:38:03 +0200

    Update main.js
    Changed localhost to VITE_API

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async (e) => {
     showSpinner();
     const data = new FormData(form);
 
-    const response = await fetch('http://localhost:8080/dream', {
+    const response = await fetch(import.meta.VITE_API + '/dream', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
