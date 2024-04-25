4504f1d4f434ed79f2da5c05980535cf3e2023a6
Bjørn <bm.saastad@gmail.com>
2024-04-25 21:21:37 +0200

    Converting base64 data to .webp format

--- server.js
+++ server.js
@@ -1,40 +1,53 @@
+import fs from "node:fs";
 import axios from "axios";
+import FormData from "form-data";
+
 import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.post('/dream', async (req, res) => {
+	console.log("--POST")
   try {
-    const formData = new FormData();
-    formData.append("prompt", "Lighthouse on a cliff overlooking the ocean");
-    formData.append("output_format", "webp");
-
-    const response = await axios.post(
+    const formData = {
+      prompt: "Lighthouse on a cliff overlooking the ocean",
+      output_format: "webp"
+    };
+    
+    const response = await axios.postForm(
       `https://api.stability.ai/v2beta/stable-image/generate/core`,
-      formData,
+      axios.toFormData(formData, new FormData()),
       {
+        validateStatus: undefined,
+        responseType: "arraybuffer",
         headers: { 
-          Authorization: `Bearer ${process.env.STABILITYAI}`, 
+          Authorization: `Bearer `+ process.env.STABILITYAI, 
           Accept: "application/json" 
         },
-        responseType: "arraybuffer"
       },
     );
     
     if(response.status === 200) {
-      res.send(response.data);
+
+		// console.log(response)q
+    //   const data = response.json()
+	console.log("---DATA")
+	console.log(response.data)
+      res.send(response.data)
+      //fs.writeFileSync("./lighthouse.webp", Buffer.from(response.data));
     } else {
       throw new Error(`${response.status}: ${response.data.toString()}`);
     }
+
   } catch (error) {
     console.error(error)
-    res.status(500).send(error?.response?.data?.error?.message || 'Something went wrong');
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
   }
 });
 
